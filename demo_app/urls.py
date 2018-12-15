7f50d775cd83cd6822df4b19fa71efe046e0d724
chezyn <9652497+chezyn@users.noreply.github.com>
2018-12-15 18:58:58 +0900

    [update] フォーム入力後に審査結果ページを表示

--- demo_app/urls.py
+++ demo_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# urlの設定
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('input_form', views.input_form, name='input_form'),
+    path('result', views.result, name='result')
 ]
--- demo_app/views.py
+++ demo_app/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import InputForm
+from .models import Customers
 
 def index(request):
     return render(request, 'demo_app/index.html', {})
+
+def input_form(request):
+    if request.method == 'POST':
+        form = InputForm(request.POST)
+        if form.is_valid(): #入力の検証
+            form.save() #入力された値の保存
+            return redirect('result') #フォーム送信のあとはリダイレクト
+
+    else:
+        form = InputForm()
+        return render(request, 'demo_app/input_form.html', {'form':form})
+
+def result(request):
+    return render(request, 'demo_app/result.html', {})
