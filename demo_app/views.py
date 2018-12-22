2c85dfd58e173249570962e6d71e64b4a452c9be
chezyn <9652497+chezyn@users.noreply.github.com>
2018-12-22 12:54:22 +0900

    補講:計算機機能の追加

--- demo_app/views.py
+++ demo_app/views.py
@@ -18,3 +18,25 @@ def input_form(request):
 
 def result(request):
     return render(request, 'demo_app/result.html', {})
+
+def calculate(request):
+    if request.method == 'POST':
+        nums = request.POST #POSTされた値の取得
+        #print(nums) #printでデバッグ(ターミナルに表示)
+        #print(nums['num1'])
+        ans = int(nums['num1']) + int(nums['num2'])
+        print(ans)
+        return render(request, 'demo_app/calculate.html', {'answer':ans}) #htmlの表示 {}に入れたものをhtmlに渡す(htmlでは'answer'で扱える)
+    else:
+        return render(request, 'demo_app/calculate.html', {}) #htmlの表示
+    #受け取ったものの演算処理
+
+    #if request.method == 'POST':
+    #    form = InputFormCalc(request.POST)
+    #    if form.is_valid():
+    #        # TODO
+    #        return redirect('calculate')
+
+    #else:
+    #    form = InputFormCalc()
+    #    return render(request, 'demo_app/calculate.html', {})
